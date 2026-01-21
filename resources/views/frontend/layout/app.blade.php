862a5493e76804088bc2838fb0c0f64050534842
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2026-01-21 23:02:07 +0600

    Page content

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Base - Multipurpose Bootstrap 5 Template</title>
+    <title>@yield('title','Blog-Practice-app')</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" type="image/x-icon" href="{{('base/img/favicon.svg')}}"/>
