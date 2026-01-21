006609b881a2fded2f3f6117f301238a716efc11
enBIJOY <162016650+enBIJOY@users.noreply.github.com>
2026-01-21 16:59:18 +0600

    jetstream middleware authentication complete

--- routes/web.php
+++ routes/web.php
@@ -21,12 +21,16 @@ Route::get('/', function (){
     return view('frontend.index');
 });
 
+//jetstream middleware
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
+    //if middleware is == authenticate(user is loged-in) and if session token is verified --
+    // -- then do next else return to login
 ])->group(function () {
     Route::get('/dashboard', function () {
         return view('dashboard');
+        //let the user retun view to dashboard
     })->name('dashboard');
 });
